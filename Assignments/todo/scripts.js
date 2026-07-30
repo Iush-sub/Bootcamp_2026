ffb345fcfd0be3b9dc909df0e394c7afaa1286ea
unknown <pas082bct004@wrc.edu.np>
2026-07-30 23:30:26 +0545

    Creating clear button

--- Assignments/todo/scripts.js
+++ Assignments/todo/scripts.js
@@ -2,13 +2,32 @@
 const inputTask=document.getElementById("task-input")
 const form=document.getElementById("task-form")
 const list=document.getElementById("task-list")
+const clearbutton=document.getElementById("cleartask")
+
 
 
 form.addEventListener("submit",(event)=>
 {
     event.preventDefault();
     const newtask=document.createElement("li")
-    newtask.textContent = inputTask.value;
+    
+    
+        
+    const checkbox=document.createElement("input")
+    checkbox.type="checkbox"
+    const span = document.createElement("span");
+    span.textContent= inputTask.value;
+    newtask.appendChild(checkbox)
+    newtask.appendChild(span)
+
     list.appendChild(newtask)
+        
+
+    
     inputTask.value="";
+})
+
+clearbutton.addEventListener("click",(event)=>
+{
+    list.innerHTML=""
 })
